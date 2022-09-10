f62773487f218b07c4674f9161f802cf9baf454e
bunssss <tkylecaluag26@gmail.com>
2022-09-09 17:54:25 -0700

    moved distube stuff from index to ready

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,11 @@
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
 const { ActivityType } = require('discord.js');
 require("dotenv").config();
+const fs = require("fs");
+const config = require('../config.json')
+const { DisTube } = require('distube')
+const { SpotifyPlugin } = require('@distube/spotify')
+const { YtDlpPlugin } = require('@distube/yt-dlp')
+const { EmbedBuilder } = require("discord.js")
 
 module.exports = {
   name: "ready",
@@ -10,12 +14,83 @@ module.exports = {
   execute(client, commands) {
     console.log('MogBot is online!');
 
+    client.on('messageCreate', async message => {
+      if (message.author.bot || !message.guild) return
+      const prefix = config.prefix
+      if (!message.content.startsWith(prefix)) return
+      const args = message.content.slice(prefix.length).trim().split(/ +/g)
+      const command = args.shift().toLowerCase()
+      const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+      if (!cmd) return
+      if (cmd.inVoiceChannel && !message.member.voice.channel) {
+        return message.channel.send(`you need to be in vc you silly goose`)
+      }
+      try {
+        cmd.run(client, message, args);
+      } catch (e) {
+        console.error(e)
+        message.channel.send('⛔ This is not a valid command')
+      }
+    })
+
+    const status = queue =>
+
+    `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
+    }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
+
+
+  client.distube
+
+    .on('playSong', (queue, song) =>
+
+      queue.textChannel.send({
+        embeds: [new EmbedBuilder()
+          .setAuthor({ name: 'Now Playing...', iconURL: 'https://c.tenor.com/SumAMhoE7EgAAAAi/among-us.gif' })
+          .setDescription(`**[${song.name}](${song.url})**`)
+          .setThumbnail(song.thumbnail)
+          .addFields(
+            { name: 'Requested By', value: `${song.member}`, inline: true },
+            { name: 'Duration', value: `${song.formattedDuration}`, inline: true }
+          )
+
+        ]
+
+      })
+    )
+
+    .on('addSong', (queue, song) =>
+      queue.textChannel.send({
+        embeds: [new EmbedBuilder()
+
+          .setDescription(`🎶  **[${song.name}](${song.url})** has been added to the Queue`)
+          .setFooter({ text: `Duration: ${song.formattedDuration}` })
+
+        ]
+      }))
+
+    .on('addList', (queue, playlist) =>
+      queue.textChannel.send(
+        `${client.emotes.success} | Added \`${playlist.name}\` playlist (${playlist.songs.length
+        } songs) to queue\n${status(queue)}`
+      )
+    )
+
+    .on('error', (channel, e) => {
+      if (channel) channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`)
+      else console.error(e)
+    })
+
+    .on('empty', channel => channel.send('amoooooooog'))
+    .on('searchNoResult', (message, query) =>
+      message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
+    )
+    
     let date = new Date()
     let day = date.getDay();
     let hour = date.getHours();
     let minutes = date.getMinutes();
 
-    function status() {
+    function statusUpdate() {
       date = new Date()
       day = date.getDay();
       hour = date.getHours();
@@ -49,7 +124,7 @@ module.exports = {
 
     }
 
-    setInterval(status, 30000)
+    setInterval(statusUpdate, 30000)
 
   }
 }
--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const { DisTube } = require('distube')
 const dotenv = require('dotenv');
 const { SpotifyPlugin } = require('@distube/spotify')
 const { YtDlpPlugin } = require('@distube/yt-dlp')
-const config = require('./con.json')
-require("./html.js");
+const config = require('./config.json')
 
 dotenv.config()
 
@@ -34,15 +33,26 @@ client.distube = new DisTube(client, {
   ]
 })
 
-const commands = [];
+    client.commands = new DiscordJS.Collection();
+    client.aliases = new DiscordJS.Collection();
+    client.emotes = config.emoji
 
-client.commands = new DiscordJS.Collection();
-client.aliases = new DiscordJS.Collection();
-client.emotes = config.emoji
+const commands = [];
 
 const eventFiles = fs
   .readdirSync("./events")
   .filter(file => file.endsWith('js'));
+
+  fs.readdir('./commands/', (err, files) => {
+    if (err) return console.log(err)
+    const jsFiles = files.filter(f => f.split('.').pop() === 'js')
+    if (jsFiles.length <= 0) return console.log('Could not find any commands!')
+    jsFiles.forEach(file => {
+      const cmd = require(`./commands/${file}`)
+      client.commands.set(cmd.name, cmd)
+    })
+    console.log("Commands Loaded!")
+  })
 
 for (const file of eventFiles) {
   const event = require(`./events/${file}`);
@@ -53,86 +63,5 @@ for (const file of eventFiles) {
     client.on(event.name, (...args) => event.execute(...args, commands));
   }
 }
-
-fs.readdir('./commands/', (err, files) => {
-  if (err) return console.log('Could not find any commands')
-  const jsFiles = files.filter(f => f.split('.').pop() === 'js')
-  if (jsFiles.length <= 0) return console.log('Could not find any commands!')
-  jsFiles.forEach(file => {
-    const cmd = require(`./commands/${file}`)
-    client.commands.set(cmd.name, cmd)
-  })
-  console.log("Commands Loaded!")
-})
-
-client.on('messageCreate', async message => {
-  if (message.author.bot || !message.guild) return
-  const prefix = config.prefix
-  if (!message.content.startsWith(prefix)) return
-  const args = message.content.slice(prefix.length).trim().split(/ +/g)
-  const command = args.shift().toLowerCase()
-  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
-  if (!cmd) return
-  if (cmd.inVoiceChannel && !message.member.voice.channel) {
-    return message.channel.send(`you need to be in vc you silly goose`)
-  }
-  try {
-    cmd.run(client, message, args);
-  } catch (e) {
-    console.error(e)
-    message.channel.send('⛔ This is not a valid command')
-  }
-})
-
-const status = queue =>
-
-
-  `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
-  }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
-
-client.distube
-
-  .on('playSong', (queue, song) =>
-
-    queue.textChannel.send({
-      embeds: [new EmbedBuilder()
-        .setAuthor({ name: 'Now Playing...', iconURL: 'https://c.tenor.com/SumAMhoE7EgAAAAi/among-us.gif' })
-        .setDescription(`**[${song.name}](${song.url})**`)
-        .setThumbnail(song.thumbnail)
-        .addFields(
-          { name: 'Requested By', value: `${song.member}`, inline: true },
-          { name: 'Duration', value: `${song.formattedDuration}`, inline: true }
-        )
-
-      ]
-    })
-  )
-
-  .on('addSong', (queue, song) =>
-    queue.textChannel.send({
-      embeds: [new EmbedBuilder()
-
-        .setDescription(`🎶  **[${song.name}](${song.url})** has been added to the Queue`)
-        .setFooter({ text: `Duration: ${song.formattedDuration}` })
-
-      ]
-    }))
-
-  .on('addList', (queue, playlist) =>
-    queue.textChannel.send(
-      `${client.emotes.success} | Added \`${playlist.name}\` playlist (${playlist.songs.length
-      } songs) to queue\n${status(queue)}`
-    )
-  )
-
-  .on('error', (channel, e) => {
-    if (channel) channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`)
-    else console.error(e)
-  })
-
-  .on('empty', channel => channel.send('amoooooooog'))
-  .on('searchNoResult', (message, query) =>
-    message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
-  )
 
 client.login(process.env.TOKEN);
